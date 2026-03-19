9e323f67364d955bc93c27bda082086d5c29f567
AlexeyBarabash <alexey@brave.com>
2026-03-19 14:54:09 +0200

    [cr148][Android] New arg hubManagerSupplier at TabbedAppMenuPropertiesDelegate.ctor
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/729996f4d16b4a3859c0c162623b7e1b1b1aea43
    
    commit 729996f4d16b4a3859c0c162623b7e1b1b1aea43
    Author: Calder Kitagawa <ckitagawa@chromium.org>
    Date:   Mon Mar 16 07:51:46 2026 -0700
    
        [Hub] Remove select_tabs from the overflow menu on non-tabs panes
    
        This also sets us up to support other "pane specific" customizations in
        the future.
    
        Fixed: 493182478
        Change-Id: Iab2f3b072ae5fffa3c55c63d8c50afe35f7285a1
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7667560
        Reviewed-by: Madhav Pruthi <madhavpruthi@google.com>
        Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
        Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
        Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
        Reviewed-by: Dan Polanco <polardz@google.com>
        Cr-Commit-Position: refs/heads/main@{#1599889}

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -1431,6 +1431,7 @@ public class BytecodeTest {
                         OneshotSupplier.class,
                         MonotonicObservableSupplier.class,
                         PageZoomManager.class,
+                        OneshotSupplier.class,
                         OpenInAppMenuItemProvider.class));
         Assert.assertTrue(
                 constructorsMatch(
