460b30ec96f67da7e0c3a5c3a77c4014089e3bf8
Aleksei Khoroshilov <5928869+goodov@users.noreply.github.com>
2026-04-29 11:22:33 +0700

    Bump commander.js to latest version. (#35933)
    * Bump commander.js to latest version.
    
    * Update gn command to work with new commander.
    
    * Fix build_config handling in various commands.

--- build/commands/scripts/commands.js
+++ build/commands/scripts/commands.js
@@ -6,7 +6,7 @@
 // Check environment before doing anything.
 import '../lib/checkEnvironment.js'
 
-import program from 'commander'
+import { program, Argument } from 'commander'
 import path from 'node:path'
 import fs from 'fs-extra'
 import config from '../lib/config.ts'
@@ -48,6 +48,18 @@ function parseInteger(string) {
 
 const parsedArgs = program.parseOptions(process.argv)
 
+function createBuildConfigArgument() {
+  // Build config argument that's valid only if it's not an option.
+  return new Argument('[build_config]', 'build configuration').argParser(
+    (value) => {
+      if (value.startsWith('-')) {
+        return undefined
+      }
+      return value
+    },
+  )
+}
+
 // @ts-ignore
 program.version(process.env.npm_package_version)
 
@@ -70,7 +82,7 @@ program
     'target environment (device, catalyst, simulator)',
   )
   .option('--checkdeps_only', 'only run checkdeps')
-  .arguments('[build_config]')
+  .addArgument(createBuildConfigArgument())
   .action(gnCheck)
 
 program
@@ -79,7 +91,7 @@ program
     '--print-patch-failures-in-json',
     'Emits a JSON structure with a list of patch files that failed to apply',
   )
-  .arguments('[build_config]')
+  .addArgument(createBuildConfigArgument())
   .action(applyPatches)
 
 program
@@ -103,7 +115,7 @@ program
     'target environment (device, catalyst, simulator)',
     /^(device|catalyst|simulator)$/i,
   )
-  .arguments('[build_config]')
+  .addArgument(createBuildConfigArgument())
   .action(async (buildConfig = config.defaultBuildConfig, options = {}) => {
     config.buildConfig = buildConfig
     if (options.target_os === 'host_os') {
@@ -231,7 +243,7 @@ program
     'PKCS11 provider configuration file path',
   )
   .option('--pkcs11-alias <alias>', 'PKCS11 key alias')
-  .arguments('[build_config]')
+  .addArgument(createBuildConfigArgument())
   .action(build)
 
 program
@@ -263,8 +275,9 @@ program
 program
   .command('start')
   .allowUnknownOption(true)
+  .allowExcessArguments(true)
   .option('-C <build_dir>', 'build config (out/Debug, out/Release')
-  .option('--v [log_level]', 'set log level to [log_level]', parseInteger, '0')
+  .option('--v [log_level]', 'set log level to [log_level]', parseInteger, 0)
   .option('--vmodule [modules]', 'verbose log from specific modules')
   .option(
     '--user_data_dir_name [base_name]',
@@ -306,7 +319,7 @@ program
     '--output_path [pathname]',
     'use the Brave binary located at [pathname]',
   )
-  .arguments('[build_config]')
+  .addArgument(createBuildConfigArgument())
   .action(start.bind(null, parsedArgs.unknown))
 
 program
@@ -360,8 +373,9 @@ program
 program
   .command('test <suite>')
   .allowUnknownOption(true)
+  .allowExcessArguments(true)
   .option('-C <build_dir>', 'build config (out/Debug, out/Release')
-  .option('--v [log_level]', 'set log level to [log_level]', parseInteger, '0')
+  .option('--v [log_level]', 'set log level to [log_level]', parseInteger, 0)
   .option('--vmodule [modules]', 'verbose log from specific modules')
   .option('--filter <filter>', 'set test filter')
   .option(
@@ -383,7 +397,7 @@ program
     '--test_launcher_jobs <test_launcher_jobs>',
     'Number of jobs to launch',
     parseInteger,
-    '4',
+    4,
   )
   .option('--target_os <target_os>', 'target OS')
   .option('--target_arch <target_arch>', 'target architecture')
@@ -427,7 +441,7 @@ program
     '26.2',
   ) // should match ios_deployment_target
   .option('--offline', 'use offline mode for RBE')
-  .arguments('[build_config]')
+  .addArgument(createBuildConfigArgument())
   .action(test.bind(null, parsedArgs.unknown))
 
 program.command('mass_rename').action(util.massRename)
@@ -445,6 +459,7 @@ program
 program
   .command('run_fuzzer <suite>')
   .allowUnknownOption(true)
+  .allowExcessArguments(true)
   .action(runFuzzer.bind(null, parsedArgs.unknown))
 
 program
@@ -452,15 +467,17 @@ program
   .option('--target_os <target_os>', 'target OS')
   .option('--target_arch <target_arch>', 'target architecture')
   .allowUnknownOption(true)
+  .allowExcessArguments(true)
   .description('Call npm run perf_tests -- --more-help for detailed help')
   .action(perfTests.runPerfTests.bind(null, parsedArgs.unknown))
 
 program
   .command('gen_gradle')
   .allowUnknownOption(true)
+  .allowExcessArguments(true)
   .option('-C <build_dir>', 'build config (out/Debug, out/Release)')
   .option('--target_arch <target_arch>', 'target architecture')
-  .arguments('[build_config]')
+  .addArgument(createBuildConfigArgument())
   .action(genGradle.bind(null, parsedArgs.unknown))
 
 program.command('docs').action(util.launchDocs)
