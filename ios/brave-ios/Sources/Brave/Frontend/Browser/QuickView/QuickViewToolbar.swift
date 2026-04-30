7b9e51fd7c8c4e994de116f8bda22ba5d6b51a99
Nuo Xu <nuoxu.nx@gmail.com>
2026-04-30 15:35:57 -0400

    [iOS] QuickView mode basic web navigation (#35824)

--- ios/brave-ios/Sources/Brave/Frontend/Browser/QuickView/QuickViewToolbar.swift
+++ ios/brave-ios/Sources/Brave/Frontend/Browser/QuickView/QuickViewToolbar.swift
@@ -8,6 +8,7 @@ import DesignSystem
 import Strings
 import SwiftUI
 import UIKit
+import Web
 
 enum QuickViewActionButton {
   case shield
@@ -30,7 +31,7 @@ class QuickViewToolbarModel {
   var isBackDisabled: Bool = true
   var isForwardDisabled: Bool = true
   var isLoading: Bool = true
-  var loadingProgress: Double = 0.2
+  var loadingProgress: Double = 0.0
   var onActionButton: ((QuickViewActionButton) -> Void)?
 
   init(
@@ -41,6 +42,42 @@ class QuickViewToolbarModel {
     self.url = url
     self.secondaryTopButton = secondaryTopButton
     self.onActionButton = onActionButton
+  }
+}
+
+extension QuickViewToolbarModel: TabObserver {
+  func tabDidUpdateURL(_ tab: some TabState) {
+    if let url = tab.visibleURL {
+      self.url = url
+    }
+  }
+
+  func tabDidStartLoading(_ tab: some TabState) {
+    isLoading = true
+  }
+
+  func tabDidStopLoading(_ tab: some TabState) {
+    isLoading = false
+  }
+
+  func tabDidChangeLoadProgress(_ tab: some TabState) {
+    loadingProgress = tab.estimatedProgress
+  }
+
+  func tabDidChangeBackForwardState(_ tab: some TabState) {
+    if let forwardListItem = tab.backForwardList?.forwardList.first,
+      forwardListItem.url.isInternalURL(for: .readermode)
+    {
+      isForwardDisabled = true
+    } else {
+      isForwardDisabled = !tab.canGoForward
+    }
+
+    isBackDisabled = !tab.canGoBack
+  }
+
+  func tabWillBeDestroyed(_ tab: some TabState) {
+    tab.removeObserver(self)
   }
 }
 
