458fd372f7b6b6ff04f90e1183d6aa98dab9640d
Oliver <oddin@brave.com>
2026-05-13 05:27:22 +0200

    [android] Implement global browser authentication lock.
    This commit introduces the ability to completely lock the browser down
    such that you must authenticate via the OS before you are able to resume
    usage. This code unifies the lock handling into a single class so that
    both the new global lock and the existing incognito feature reuse the
    same code. Unit tests have also been added to verify behaviour.
    
    I have made a number of design decision which I hope have been
    carefully thought out - they are as follows:
    
    1. Locking private tabs is orthogonal and maintains its own auth.
    
       Rationale:
         If a user hands their device to someone else to look at a regular
         browser page, they would likely prefer if that person cannot switch
         over to their private tabs without authentication.
    
    2. Locking down the whole browser prevents Android showing the app's
       viewport in the running app switcher.
    
       Rationale:
         This would surely constitute an information leak should someone
         scroll through the OS list of running apps and be able to see what
         the user had open in Brave despite it being locked.
    
    3. Removing all usable credentials from the OS does not unlock the
       browser.
    
       Rationale:
         The user can re-add any credential in order to regain access to the
         browser again.
    
    4. Keep the global browser authentication when returning to a locked
       private tab.
    
       Rationale:
         If the user returns and a private tab or the private tab auth
         screen is focused, I could not find a clean, foolproof way of
         knowing that the user backed out of it without bothering to
         authenticate. In the absence of such a method, this can be abused
         as a bypass. Consequently, double-authentication happens in this
         case - first globally, then the user must tap to unlock the private
         tabs.
    
    5. Don't prevent the user from taking screenshots with the global lock
       on.
    
       Rationale:
         A user is far more likely to want to screenshot their regular
         browser windows than not. If they are authenticated there is no
         reason to stop them. If we were going to do this, it would make
         more sense to have it as its own separate setting.

--- android/java/org/chromium/base/BravePreferenceKeys.java
+++ android/java/org/chromium/base/BravePreferenceKeys.java
@@ -55,6 +55,7 @@ public final class BravePreferenceKeys {
     public static final String BRAVE_DEFERRED_DEEPLINK_VPN = "brave_deferred_deeplink_vpn";
     public static final String BRAVE_CLOSE_TABS_ON_EXIT = "close_tabs_on_exit";
     public static final String BRAVE_CLEAR_ON_EXIT = "clear_on_exit";
+    public static final String BRAVE_BROWSER_LOCK = "browser_lock";
     public static final String BRAVE_QUICK_ACTION_SEARCH_AND_BOOKMARK_WIDGET_TILES =
             "org.chromium.chrome.browser.widget.quickactionsearchandbookmark.QuickActionSearchAndBookmarkWidgetProvider.TILES";
     public static final String ENABLE_MULTI_WINDOWS = "enable_multi_windows";
